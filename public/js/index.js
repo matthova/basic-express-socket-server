82afbd6304defc83ef8dcaa6ce13e32ab0cf15b2
matthova <matthova87@gmail.com>
2016-05-25 15:21:39 -0700

    added option for color rainbow

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,8 @@ $(document).ready( function() {
   $("#the-time").text(new Date());
   var socket = io.connect('/');
   socket.on('update', function (data) {
+    // var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    // $('body').css("background-color", hue);
     $("#the-time").text(new Date());
   });
 });
